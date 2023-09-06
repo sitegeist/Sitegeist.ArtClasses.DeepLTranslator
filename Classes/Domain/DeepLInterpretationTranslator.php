ac3606f4077237e8b52a27b5a9730b98a51841b4
Bernhard Schmitt <schmitt@sitegeist.de>
2023-09-06 10:17:29 +0200

    Translate additional image interpretation text properties

--- Classes/Domain/DeepLInterpretationTranslator.php
+++ Classes/Domain/DeepLInterpretationTranslator.php
@@ -11,6 +11,8 @@ namespace Sitegeist\ArtClasses\DeepLTranslator\Domain;
 use DeepL\Translator;
 use Neos\Flow\I18n\Locale;
 use Sitegeist\ArtClasses\Domain\Interpretation\ImageInterpretation;
+use Sitegeist\ArtClasses\Domain\Interpretation\InterpretedObject;
+use Sitegeist\ArtClasses\Domain\Interpretation\InterpretedText;
 use Sitegeist\ArtClasses\Domain\Translation\ImageInterpretationTranslatorInterface;
 
 /**
@@ -28,17 +30,76 @@ final class DeepLInterpretationTranslator implements ImageInterpretationTranslat
         ?Locale $sourceLocale,
         Locale $targetLocale
     ): ImageInterpretation {
+        $textsToTranslate = [];
+        $translationIndices = [];
+        $i = 0;
+        if ($imageInterpretation->description) {
+            $textsToTranslate[] = $imageInterpretation->description;
+            $translationIndices['description'] = $i;
+            $i++;
+        }
+        foreach ($imageInterpretation->labels as $interpretedLabel) {
+            $textsToTranslate[$i] = $interpretedLabel;
+            $translationIndices['labels'][] = $i;
+            $i++;
+        }
+        foreach ($imageInterpretation->objects as $interpretedObject) {
+            $textsToTranslate[$i] = $interpretedObject->name;
+            $translationIndices['objects'][] = $i;
+            $i++;
+        }
+        foreach ($imageInterpretation->texts as $interpretedText) {
+            $textsToTranslate[$i] = $interpretedText->text;
+            $translationIndices['texts'][] = $i;
+            $i++;
+        }
+
         $translatedValues = $this->translator->translateText(
-            [
-                $imageInterpretation->description
-            ],
+            $textsToTranslate,
             $sourceLocale?->getLanguage(),
             $targetLocale->getLanguage()
         );
 
+        $description = array_key_exists('description', $translationIndices)
+            ? $translatedValues[$translationIndices['description']]
+            : $imageInterpretation->description;
+
+        $labels = $imageInterpretation->labels;
+        foreach ($translationIndices['labels'] ?? [] as $labelIndex => $translationIndex) {
+            $labels[$labelIndex] = $translatedValues[$translationIndex]->text;
+        }
+
+        $objects = $imageInterpretation->objects;
+        foreach ($translationIndices['objects'] ?? [] as $labelIndex => $translationIndex) {
+            $object = $objects[$labelIndex];
+            $objects[$labelIndex] = new InterpretedObject(
+                $translatedValues[$translationIndex]->text,
+                $object->boundingPolygon
+            );
+        }
+
+        $texts = $imageInterpretation->texts;
+        $additionalTexts = [];
+        foreach ($translationIndices['texts'] ?? [] as $labelIndex => $translationIndex) {
+            $text = $objects[$labelIndex];
+            $additionalTexts[] = new InterpretedText(
+                $translatedValues[$translationIndex]->text,
+                null,
+                $text->boundingPolygon
+            );
+        }
+
         return new ImageInterpretation(
             $targetLocale,
-            $translatedValues[0]->text ?? $imageInterpretation->description
+            $description,
+            $labels,
+            $objects,
+            array_merge(
+                $texts,
+                $additionalTexts
+            ),
+            $imageInterpretation->dominantColors,
+            $imageInterpretation->cropHints
         );
     }
 }
